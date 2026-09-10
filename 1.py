6e0a3a7084de3cb7a1a09bc6548fbec556679bbd
Bareera-Maheen <168240951+Bareera-Maheen@users.noreply.github.com>
2026-09-11 00:47:18 +0500

    first commit

--- 1.py
+++ 1.py
@@ -22,4 +22,48 @@ print(name1)
 #  intiger is number 
 # float is the decimal numbers 
 # boolean is the true or false value 
-# 
+# list is collection of values that is orderd changeable, and ca contain dublicate values 
+# and for it listed values we use the square brackets [] and after the each value we use come to separat them , and value can be doubled or repeted.
+# example =
+grocery= ["eggs","flour","daal","chicken poks" ,"cooking oil","sugar","salt"]
+print(grocery)
+# tuple in this dta type we can insert the multiple  value as we do in the list but whats the difference the difference is it can be written in small brackets and not changeable rest is same 
+veges=("tomatoes","potatoes","green chilli","corriander","garlic","ginger","mint","carrots")
+print(veges)
+# set is the data type that does not carry same values and does not have indexing , but have the option of changeable value and we store the values  in curly braces {}
+fruits={"mangoes","oranges","mangoes","bananas","pomegrants","pineapple"}
+print(fruits)
+# a dictionary is a data type in which e store the data in key-value pairs 
+# key:value like we wer doing in variables and and its value 
+# but in dictionary we can store more data and whole data named with other name but we also have to focus on key that we have to put key in inverted commas
+# example
+Bareerah={
+    "name":"Bareerah",
+    "Age":23,
+    "qualification":"graduated"
+
+}
+print(Bareerah)
+
+# the none data type which has no value , we just have the variable have not any value, but we can store the value later 
+shoping_list=None
+print(shoping_list)
+
+# operators
+# arithematics=addition ,subtraction,multiplication,division
+num1=2+2
+print(num1)
+integer1=3-1
+print(integer1)
+numerical1=3*8
+print(numerical1)
+float1=31.8/6
+print(float1)
+
+# comparision
+# greater then ,less then, == is equal, != not equal 
+
+# logical 
+# and , or not 
+# assignment =, +=,-=
+
