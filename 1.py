63bbf21bcbe44624644aca91c1dc34483891b8a8
Bareera-Maheen <168240951+Bareera-Maheen@users.noreply.github.com>
2026-09-10 23:36:54 +0500

    first commit

---
+++ 1.py
@@ -0,0 +1,25 @@
+print("hi")
+name="hi"
+print(name)
+
+number=2
+print(number)
+name="Bareerah Maheen"
+
+print(name)
+
+
+# a variable in pyhon is a name used to store a value
+#  means its a contaier or like box in which we store the data  
+#  example
+name1="mahi"
+# name1= this the container or box which we called the variable 
+# 'mahi' will be the value the thing we are storing in the box
+print(name1)
+
+# Data types
+#  string is text 
+#  intiger is number 
+# float is the decimal numbers 
+# boolean is the true or false value 
+# 
